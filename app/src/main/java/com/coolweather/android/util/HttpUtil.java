9239d975d25a39ccebc8f4914cc346238153518a
NicoLiutong <1299924752@qq.com>
2017-01-31 22:27:24 +0800

    2

--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -1,8 +1,17 @@
 package com.coolweather.android.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by 刘通 on 2017/1/31.
  */
 
 public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
